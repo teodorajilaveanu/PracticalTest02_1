7f8587d1e5e4fd660ed2780274300b6386d104b6
Jilaveanu Teodora <teodora.jilaveanu@gmail.com>
2022-05-26 11:10:14 +0300

    2, 3

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/MainActivity.java
@@ -3,12 +3,64 @@ package ro.pub.cs.systems.eim.practicaltest02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText port;
+    EditText client_port;
+    EditText ip;
+    EditText key;
+    EditText value;
+    Button get;
+    Button put;
+    Button start_server;
+    TextView result;
+    ServerThread serverThread;
+    ClientThread clientThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        port = findViewById(R.id.server_port);
+        start_server = findViewById(R.id.start_server);
+        client_port = findViewById(R.id.client_port);
+        ip = findViewById(R.id.client_addr);
+        key = findViewById(R.id.key);
+        value = findViewById(R.id.value);
+        get = findViewById(R.id.get);
+        put = findViewById(R.id.put);
+        result = findViewById(R.id.result);
+
+        start_server.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                serverThread = new ServerThread(Integer.parseInt(port.getText().toString()));
+                serverThread.start();
+            }
+        });
+
+        put.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String query = "put," + key.getText().toString()+","+value.getText().toString()+"\n";
+                clientThread = new ClientThread(Integer.parseInt(client_port.getText().toString()), ip.getText().toString(), query, result);
+                clientThread.start();
+            }
+        });
+
+        get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    String query = "get," + key.getText().toString()+"\n";
+                    clientThread = new ClientThread(Integer.parseInt(client_port.getText().toString()), ip.getText().toString(), query, result);
+                    clientThread.start();
+            }
+        });
+
+
     }
+
 }
